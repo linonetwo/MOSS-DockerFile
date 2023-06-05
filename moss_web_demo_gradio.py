f3eebc8607a90220cdc0c3ade13a10f9375e888b
lin onetwo <linonetwo012@gmail.com>
2023-06-05 15:10:09 +0800

    Update moss_web_demo_gradio.py
    fixes https://github.com/linonetwo/MOSS-DockerFile/issues/7

--- moss_web_demo_gradio.py
+++ moss_web_demo_gradio.py
@@ -180,4 +180,4 @@ with gr.Blocks() as demo:
 
     emptyBtn.click(reset_state, outputs=[chatbot, history], show_progress=True)
 
-demo.queue().launch(share=False, inbrowser=True)
+demo.queue().launch(share=False, inbrowser=True, server_name="0.0.0.0", server_port=7860)
